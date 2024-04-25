4ec6340cbd707c5a139e786bc3462a7c39cf5de1
Павел Борисов <perevod-avtozavod@yandex.ru>
2024-04-25 05:55:50 +0300

    Сделал программу надёжнее, добавил в копирование стиль зачёркнутый текст и цвет текста

--- 714.py
+++ 714.py
@@ -4,6 +4,7 @@ from docx.shared import Pt
 # Открываем документ
 doc = Document('template.docx')
 
+
 def search_redact_paragraph(index):
     paragraph = doc.paragraphs[index]
     print([run.text for run in paragraph.runs])
@@ -12,8 +13,9 @@ def search_redact_paragraph(index):
         if run_text.startswith('<') and run_text.endswith('>'):
             indexes.append(index)
 
+
 def redact_paragraph(index, line):
-    # Получаем первый параграф
+    # Получаем параграф
     flag = False
     paragraph = doc.paragraphs[index]
     p_style = paragraph.style
@@ -24,14 +26,13 @@ def redact_paragraph(index, line):
     new_paragraph = doc.add_paragraph(style=p_style)
     new_paragraph.paragraph_format.line_spacing = line_spacing
     new_paragraph.alignment = p_alignment
-    if new_paragraph.alignment:
-        print('True')
-
 
     # Проходимся по всем Run в текущем параграфе
     for run in paragraph.runs:
         run_text = run.text.strip()
-        if (not run_text.startswith('<') and not run_text.endswith('>')) or flag:
+        if (
+            not run_text.startswith('<') and not run_text.endswith('>')
+        ) or flag:
             # Копируем Run в новый параграф
             new_run = new_paragraph.add_run(run.text)
             # Копируем форматирование
@@ -39,27 +40,48 @@ def redact_paragraph(index, line):
             new_run.italic = run.italic
             new_run.underline = run.underline
             new_run.font.size = run.font.size
+            # print(new_run.font.size)
         else:
-            #print('Мы нашли искомый ран!')
             flag = True
             print(run.text)
             new_run = new_paragraph.add_run()
+            new_run = new_paragraph.add_run(line)
             # Копируем форматирование
-            new_run.bold = run.bold
             new_run.italic = run.italic
             new_run.underline = run.underline
             new_run.font.size = run.font.size
-            new_run = new_paragraph.add_run(line)
-            new_run.bold = True
-            new_run.font.size = Pt(15)
-
+            # копируем цвет текста
+            color = run.font.color.rgb
+            if color is not None:
+                # устанавливаем цвет текста
+                new_run.font.color.rgb = color
+            new_run.bold = run.bold
+            # проверяем зачёркивание рана
+            strike = run.font.strike
+            # Устанавливаем значение зачеркивания для нового Run такое же,
+            # как у текущего Run
+            new_run.font.strike = strike
+            if run.text[1:-1]:
+                try:
+                    f_s, new_run.bold = [
+                        int(number) for number in run.text[1:-1].replace(
+                            ' ', '').split(',')]
+                    new_run.font.size = Pt(f_s)
+                except ValueError:
+                    array = [
+                        int(number) for number in run.text[1:-1].replace(
+                            ' ', ''
+                            ).split(',')]
+                    if len(array) != 2:
+                        print(f'Ран {run.text} должен содержать '
+                              f'два целых числа, разделённых запятой')
 
     # Вставляем новый параграф перед текущим
-    index = list(doc.element.body).index(paragraph._element)
     doc.element.body.insert(index, new_paragraph._element)
 
     # Удаляем исходный параграф
     doc.element.body.remove(paragraph._element)
+
 
 indexes = []
 index = 0
@@ -88,7 +110,6 @@ for field in fields:
 
 print('----------------', lines)
 
-#lines = ["АЗЕРБАЙДЖАНСКАЯ РЕСПУБЛИКА ИРАН", "Иванова", "Глиссандра", "Рюриковна"]
 for index in range(len(doc.paragraphs)):
     search_redact_paragraph(index)
 
@@ -104,4 +125,3 @@ doc = Document('test3.docx')
 for index in range(len(doc.paragraphs)):
     if search_redact_paragraph(index):
         indexes.append(index)
-
